8a04ded70d47761334f1093989c270753e51b063
sandfm118 <azhartamboli2004@gamil.com>
2024-07-12 16:11:29 +0530

    09

---
+++ js/js.js
@@ -0,0 +1,171 @@
+
+
+/******************************************/
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	'use strict';
+	Splitting();
+	luxy.init();
+	gsap.registerPlugin(ScrollTrigger);
+
+	const gTl = gsap.timeline();
+	gTl.from(".title .char", 1, { opacity: 0, yPercent: 130, stagger: 0.06, ease: "back.out" });
+	gTl.to(".header__video", 2, { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", scale: 1, ease: "expo.out" }, "-=1");
+	gTl.from(".header__marq", 2, { opacity: 0, yPercent: 100, ease: "expo.out" }, "-=1.5");
+
+	const gsapSq = gsap.utils.toArray('.section-title__square');
+	gsapSq.forEach((gSq, i) => {
+		const rotat = gsap.from(gSq, 3, { rotation: 720 });
+		ScrollTrigger.create({
+			trigger: gSq,
+			animation: rotat,
+			start: 'top bottom',
+			scrub: 1.9
+		});
+	});
+
+
+	//header
+	function header() {
+		gsap.to('.title_sandf', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			yPercent: -150
+		})
+		gsap.to('.header .stroke', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			xPercent: 50
+		})
+		gsap.to('.header__video', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			xPercent: -70
+		})
+		gsap.to('.header__video #video', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			scale: 1.3
+		})
+		gsap.to('.header__marq-wrapp', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			xPercent: -50
+		})
+		gsap.to('.header__marq-star img', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			rotate: -720
+		})
+	}
+	header();
+
+
+	//about
+	function about() {
+		gsap.from('.about__img', {
+			scrollTrigger: {
+				trigger: '.about',
+				start: 'top bottom',
+				scrub: 1.9
+			},
+			yPercent: 80
+		})
+		gsap.from('.about__img img', {
+			scrollTrigger: {
+				trigger: '.about',
+				start: 'top bottom',
+				scrub: 1.9
+			},
+			scale: 1.6
+		})
+		gsap.to('.about__txt', {
+			scrollTrigger: {
+				trigger: '.about__wrapp',
+				start: 'top bottom',
+				scrub: 1.9
+			},
+			yPercent: 50
+		})
+	}
+	about();
+
+
+	//skill
+	function skill() {
+		gsap.from('.skill__num', {
+			x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+			scrollTrigger: {
+				trigger: '.skill__list',
+				start: 'top bottom',
+				scrub: 1.9
+			}
+		})
+	}
+	skill();
+
+
+	//portfolio
+	function portfolio() {
+		gsap.from('.work__item, .work__item-num', {
+			y: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+			scrollTrigger: {
+				trigger: '.work',
+				start: 'top bottom',
+				scrub: 1.9
+			}
+		})
+		gsap.from('.work__item-img img', {
+			scale: 1.6,
+			scrollTrigger: {
+				trigger: '.work__wrapp',
+				start: 'top bottom',
+				scrub: 1.9
+			}
+		})
+	}
+	portfolio();
+
+
+	//link
+	function link() {
+		gsap.from('.link__item-arrow', {
+			x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+			scrollTrigger: {
+				trigger: '.link__list',
+				start: 'top bottom',
+				scrub: 1.9
+			}
+		})
+	}
+	link();
+	
+
+});
+
+
+
+
+
+
+
+
